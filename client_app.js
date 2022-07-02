e4db603546848be86d8874006d37b7d3ee2ceaba
mrc-sprinklr <mayukh.raychaudhuri@sprinklr.com>
2022-07-02 23:24:51 +0530

    updated

--- client_app.js
+++ client_app.js
@@ -3,7 +3,7 @@ const { CallerPackage } = require("./CallerPackage/client/client.js");
 let callerPackage = new CallerPackage();
 
 document.getElementById("call-button").addEventListener("click", () => {
-  callerPackage.call("4153260912");
+  callerPackage.call("4157614983");
 });
 
 document.getElementById("end-button").addEventListener("click", () => {
